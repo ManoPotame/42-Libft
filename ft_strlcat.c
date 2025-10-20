09e9b7ca7b114794f035c485680092f8f19430eb
Matheo Haouas <mhaouas@2a02-842a-026e-2801-a260-974f-0b93-8665.rev.sfr.net>
2025-10-20 17:23:07 +0200

    Adding a main test to strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mcrenn <mcrenn@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 17:05:37 by mcrenn            #+#    #+#             */
-/*   Updated: 2025/10/16 17:43:58 by mcrenn           ###   ########.fr       */
+/*   Updated: 2025/10/19 18:06:54 by mcrenn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,12 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	}
 	dest[i] = '\0';
 	return (ft_strlen(dest));
+}
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	//printf("%d\n", ft_strlcat("pouette", "pouet", 8));
+	printf("%d", strlcat("pouette", "pouet", 8)); //Max 8, cat plus len:12
 }
