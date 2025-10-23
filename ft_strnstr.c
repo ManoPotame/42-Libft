fccabe70f9871ca108b03e14ef38641fa3fc83d2
Manolo Crenn <mcrenn@2D7.42angouleme.fr>
2025-10-23 10:34:30 +0200

    Adding function ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcrenn <mcrenn@student.42angouleme.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 14:53:51 by mcrenn            #+#    #+#             */
+/*   Updated: 2025/10/23 10:32:51 by mcrenn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (little[0] == '\0')
+	{
+		return (&big[0]);
+	}
+	if (big[0] == '\0')
+		return (NULL);
+	while (i < len)
+	{
+		j = 0;
+		while ((little[j] == big[i + j] && i + j < len) || little[j] == '\0')
+		{
+			if (little[j] == '\0')
+				return (&big[i]);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
