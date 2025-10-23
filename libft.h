5c5657beefcb0187d4ae1dbaf0632a4006cd5f79
Manolo Crenn <mcrenn@2C1.42angouleme.fr>
2025-10-23 23:56:36 +0200

    Adding the gitignore. All the basical functions are finished and norme is valid

--- libft.h
+++ libft.h
@@ -6,44 +6,43 @@
 /*   By: mcrenn <mcrenn@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 18:04:51 by mcrenn            #+#    #+#             */
-/*   Updated: 2025/10/23 10:44:40 by mcrenn           ###   ########.fr       */
+/*   Updated: 2025/10/23 23:50:44 by mcrenn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+# include <unistd.h>
 
-#include <unistd.h>
+int			ft_isalpha(int character);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
 
-int	ft_isalpha(int character);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+size_t		ft_strlen(const char *str);
 
-size_t	ft_strlen(const char *str);
+void		*ft_memset(void *s, int c, size_t n);
+void		ft_bzero(void *s, size_t n);
+void		*ft_memcpy(void *dest, const void *src, size_t n);
+void		*ft_memmove(void *dest, const void *src, size_t n);
 
-void	*ft_memset(void *s, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t		ft_strlcpy(char *dst, const char *src, size_t size);
+size_t		ft_strlcat(char *dest, const char *src, size_t size);
 
-size_t ft_strlcpy(char *dst, const char *src, size_t size);
-size_t	ft_strlcat(char *dest, const char *src, size_t size);
+int			ft_toupper(int c);
+int			ft_tolower(int c);
 
-int	ft_toupper (int c);
-int	ft_tolower (int c);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strrchr(const char *s, int c);
 
-char *ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+void		*ft_memchr(const void *s, int c, size_t n);
 
-void	*ft_memchr(const void *s, int c, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
 
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
+char		*ft_strnstr(const char *big, const char *little, size_t len);
 
-char *ft_strnstr(const char *big, const char *little, size_t len);
-
-int	ft_atoi(const char *nptr);
+int			ft_atoi(const char *nptr);
 #endif
